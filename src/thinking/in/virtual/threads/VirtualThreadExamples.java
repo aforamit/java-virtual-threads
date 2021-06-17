7f71e2be29ce36927ca3e0ed7f1c17e609cd3bfe
aforamit <aforamit@hotmail.com>
2021-06-18 04:58:27 +0530

    adding java virtual threads code samples

--- src/thinking/in/virtual/threads/VirtualThreadExamples.java
+++ src/thinking/in/virtual/threads/VirtualThreadExamples.java
@@ -11,18 +11,32 @@ import static thinking.in.virtual.threads.Utility.timed;
 
 public class VirtualThreadExamples {
 
-    static class Example1ThreadAPI {
+    static class Example0ThreadAPI {
         public static void main(String[] args) throws InterruptedException {
 
             Thread.startVirtualThread(
-                    () -> System.out.println("Project Loom is here!! : " + Thread.currentThread()));
+                    () -> System.out.println("Project Loom is here!! : "
+                            + Thread.currentThread()))
+                    .join();
+        }
+    }
 
-            Thread.builder().name("virtual-thread-")
+    static class Example1ThreadAPI {
+        public static void main(String[] args) throws InterruptedException {
+            Thread vThread = Thread.builder().name("virtual-thread-")
                     .virtual()
-                    .task(() -> System.out.println("ThreadBuilder is fun!! : " + Thread.currentThread())).build().start();
+                    .task(() -> System.out.println("ThreadBuilder is fun!! : "
+                            + Thread.currentThread()))
+                    .build();
+            vThread.start();
+            vThread.join();
 
-            Thread.builder().name("kernel-thread-")
-                    .task(() -> System.out.println("ThreadBuilder is fun!! : " + Thread.currentThread())).build().start();
+            Thread kThread = Thread.builder().name("kernel-thread-")
+                    .task(() -> System.out.println("ThreadBuilder is fun!! : "
+                            + Thread.currentThread()))
+                    .build();
+            kThread.start();
+            kThread.join();
 
             Thread.sleep(Duration.ofSeconds(1));
             System.out.println("Exiting now...");
@@ -31,19 +45,21 @@ public class VirtualThreadExamples {
 
     static class Example2CarrierThread {
         public static void main(String[] args) throws InterruptedException {
-            var factory = Thread.builder().name("kernel-carrier-thread").daemon(true).factory();
-            //var factory = Thread.builder().name("virtual-thread").virtual().factory(); //throws IllegalCallerException
+            var factory = Thread.builder().name("kernel-carrier-thread")
+                    .daemon(true).factory();
+            //var factory = Thread.builder().name("virtual-thread")
+            //              .virtual().factory(); //throws IllegalCallerException
 
             //this executor creates ONLY 1 kernel thread
             var executor = Executors.newSingleThreadExecutor(factory);
 
             Thread vThread = Thread.builder().name("virtual-thread-")
                     .virtual(executor)
-                    .task(() -> System.out.println("Virtual Thread with Carrier Thread!! : " + Thread.currentThread()))
+                    .task(() -> System.out.println("Virtual Thread with Carrier Thread!! : "
+                            + Thread.currentThread()))
                     .build();
             vThread.start();
-
-            Thread.sleep(Duration.ofSeconds(1));
+            vThread.join();
             System.out.println("Exiting now...");
         }
     }
@@ -51,11 +67,12 @@ public class VirtualThreadExamples {
     static class Example3KernelThreadBlocking {
         public static void main(String[] args) {
             timed(o -> {
-                var factory = Thread.builder().name("kernel-thread").daemon(false).factory();
+                var factory = Thread.builder().name("kernel-thread")
+                        .daemon(false).factory();
 
                 try (var ex = Executors.newSingleThreadExecutor(factory)) {
                     IntStream.range(0, 5)
-                            .forEach(i -> ex.execute(sleepingTask(i)));
+                            .forEach(i -> ex.execute(sleepTask(i)));
                 }
                 System.out.println("Exiting now...");
             });
@@ -72,12 +89,13 @@ public class VirtualThreadExamples {
                 ExecutorService kernelThreadExecutor = singleThreadKernelExecutor(); //creates 1 kernel thread
 
                 var virtualThreadFactory = Thread.builder()
-                        .name("virtual-thread-", 0).virtual(kernelThreadExecutor).factory();
+                        .name("virtual-thread-", 0)
+                        .virtual(kernelThreadExecutor).factory();
 
                 //ExecutorService create  a new Virtual Thread for every task
                 try (var ex = Executors.newThreadExecutor(virtualThreadFactory)) {
                     IntStream.range(0, 5)
-                            .forEach(i -> ex.execute(sleepingTask(i)));
+                            .forEach(i -> ex.execute(sleepTask(i)));
                 }
                 //ex.shutdown();
                 //ex.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
@@ -86,22 +104,25 @@ public class VirtualThreadExamples {
         }
 
         private static ExecutorService singleThreadKernelExecutor() {
-            var kernelThreadFactory = Thread.builder().name("kernel-carrier-thread-", 0).daemon(false).factory();
+            var kernelThreadFactory = Thread.builder()
+                    .name("kernel-carrier-thread-", 0)
+                    .daemon(false).factory();
             return Executors.newSingleThreadExecutor(kernelThreadFactory);
         }
     }
 
-    private static Runnable sleepingTask(int id) {
+    private static Runnable sleepTask(int id) {
         return () -> {
-            System.out.println(format("[%s][%s] Starting time consuming task [id=%s]", now(), Thread.currentThread(), id));
+            System.out.println(format("[%s][%s] Starting time consuming task [id=%s]",
+                    now(), Thread.currentThread(), id));
             try {
                 Thread.sleep(Duration.ofSeconds(5));
             } catch (InterruptedException e) {
-                System.out.println(format("[%s][%s] InteruptedException occurred time consuming task [id=%s]", now(), Thread.currentThread(), id));
+                System.out.println(format("[%s][%s] InteruptedException occurred time consuming task [id=%s]",
+                        now(), Thread.currentThread(), id));
             }
-            System.out.println(format("[%s][%s] Ended time consuming task [id=%s]", now(), Thread.currentThread(), id));
+            System.out.println(format("[%s][%s] Ended time consuming task [id=%s]",
+                    now(), Thread.currentThread(), id));
         };
     }
-
-
 }
